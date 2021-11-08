39443ca21f45e003082aca3eecf3cea84edf9f5d
Juan Balseca <jebalsec@pichincha.com>
2021-11-08 14:30:18 -0500

    Change text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-
 export default () => {
     return <div>
-        <h1>Hi there!</h1>
+        <h1>Hi there!!!!</h1>
         <hr />
         <MarketingApp/>
     </div>
